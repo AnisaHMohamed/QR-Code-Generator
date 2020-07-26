59283b6703f7f0d850e0016fcb6192cee97538dd
Anisa <anisahamohamed@gmail.com>
2020-07-26 10:08:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const qrGenerator = (url) => {
+    var typeNumber = 4;
+    var errorCorrectionLevel = "L";
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData(url);
+    qr.make();
+    document.getElementById("placeHolder").innerHTML = qr.createImgTag();
+  };
+
   return (
     <div className="App">
+      <script type="text/javascript" src="qrcode.js"></script>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div id="placeHolder"></div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
