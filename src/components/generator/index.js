bb61cf87e5a25765a5f2f0c60bb2f2b93a3a2015
Anisa <anisahamohamed@gmail.com>
2020-07-26 10:44:04 -0400

    qr generator state and form

---
+++ src/components/generator/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+// Actions
+import { addQRCode, getQRCodes } from "../actions";
+
+const AddQRCode = ({ addQRCode, getQRCodes }) => {
+  const [code, setCode] = useState({ qr_code: "", link: "" });
+
+  return (
+    <>
+      <h3>Link</h3>
+      <input onChange={(e) => setDetail({ ...detail, link: e.target.value })} />
+      <h3>Genre</h3>
+      <input onChange={(e) => setCode({ ...code, link: e.target.value })} />
+      <br />
+      <button
+        style={{ marginTop: 20 }}
+        onClick={() => {
+          addQRCode(code);
+          getQRCodes();
+        }}
+      >
+        Create a new QR code!
+      </button>
+    </>
+  );
+};
+
+const mapDispatchToProps = {
+  addBook,
+  getBooks,
+};
+
+export default connect(null, mapDispatchToProps)(AddBook);
